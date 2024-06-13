6cd5ca9e49ead592abbe067c92f792c8eb5b477b
leoklems <leoklems@yahoo.com>
2024-06-13 13:39:00 +0100

    added user model

--- users/models.py
+++ users/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    
+    def __str__(self):
+        return f"{self.email}"
+
 
 class HealthExpert(models.Model):
     city = models.CharField(max_length=100)
@@ -6,21 +13,21 @@ class HealthExpert(models.Model):
     is_verified = models.BooleanField(default=False)
     is_avaliable = models.BooleanField(default=False)
     last_avaliable_date = models.DateTimeField(null=True)
-    display_image = models.ImageField(upload_to='images/')
+    display_image = models.ImageField(upload_to='images/', null=True)
     date_joined = models.DateTimeField(auto_now_add=True)
 
 class Patient(models.Model):
     city = models.CharField(max_length=100)
     gender = models.CharField(max_length=100)
     last_login = models.DateTimeField(null=True)
-    display_image = models.ImageField(upload_to='images/')
+    display_image = models.ImageField(upload_to='images/', null=True)
     date_joined = models.DateTimeField(auto_now_add=True)
 
 class Staff(models.Model):
     city = models.CharField(max_length=100)
     gender = models.CharField(max_length=100)
     last_login = models.DateTimeField(null=True)
-    display_image = models.ImageField(upload_to='images/')
+    display_image = models.ImageField(upload_to='images/', null=True)
     date_joined = models.DateTimeField(auto_now_add=True)
 
 
