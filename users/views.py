b87d436fd74e42eca409eaceb07c8c03494a650c
leoklems <leoklems@yahoo.com>
2024-06-14 15:38:29 +0100

    added a view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse, FileResponse, HttpResponse, HttpResponseRedirect, HttpRequest
+from django.views.generic import DetailView, View
 
 # Create your views here.
+
+class Home(View):
+    def get(self, request):
+        response = HttpResponse("Site is running") 
+        return response
