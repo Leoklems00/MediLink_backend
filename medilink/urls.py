56f6f4db9db84758d142e1faf5b318970285467b
leoklems <leoklems@yahoo.com>
2024-06-17 16:55:16 +0100

    fixed home

--- medilink/urls.py
+++ medilink/urls.py
@@ -22,8 +22,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("users.urls")),
+    
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("", include("users.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Expert, Patient, Staff
 
-class HealthExpertSerializer(serializers.ModelSerializer):
+class ExpertSerializer(serializers.ModelSerializer):
     class Meta:
         model = Expert
         fields = '__all__'
