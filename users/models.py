29dbe35f0475eb8c21e217a6094e377f8c9164e5
leoklems <leoklems00@gmail.com>
2024-06-19 21:53:09 +0100

    Revert "feat: auth user"
    This reverts commit 45b5bb66a9085d5e7d463a28f2974ded11a0efd6.
    
    gcjgcjhghcxb::wq

--- users/models.py
+++ users/models.py
@@ -1,19 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
-class CustomUser(AbstractUser):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50, unique=True)
-    phone_number = models.CharField(max_length=15)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-def __str__(self):
-    return self.email
-
+# class User(AbstractUser):
+    
+#     def __str__(self):
+#         return f"{self.email}"
+#
 
 class Specialty(models.Model):
     name = models.CharField(max_length=50)
