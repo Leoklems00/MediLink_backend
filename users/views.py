403eff0962397343dbf0cbffd798752a596a0146
leoklems <leoklems00@gmail.com>
2024-06-21 02:18:56 +0100

    login enpoint fix

--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import *
 from rest_framework.response import Response
-from djangorestframework_simplejwt import authentication
+# from djangorestframework_simplejwt import authentication
 
 from .serializers import *
 
